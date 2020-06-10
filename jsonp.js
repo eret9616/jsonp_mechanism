35b9884673345bbca157f8920ea669539cfd1713
huhao <947416983@qq.com>
2020-06-10 15:08:08 +0800

    .

--- jsonp.js
+++ jsonp.js
@@ -1,15 +1,20 @@
 /*
 手写一个JSONP（promise版）
-·一句话阐述下JSONP原理：
+
+JSONP原理：
+利用js标签没有跨域限制的特性实现跨域请求 只支持GET
+
+
 动态生成一个JavaScript标签，其src由接口url、请求参数、callback函数名拼接而成，
-利用js标签没有跨域限制的特性实现跨域请求。
 
-·有几点需要注意：
-1.callback函数要绑定在window对象上
-2.服务端返回数据有特定格式要求：callback函数名+'('+JSON.stringify(返回数据)+')'
-3.不支持post，因为js标签的src属性本身就是一个get请求
+1.创建一个script标签，src是请求url+qs.stringify(data)+?callback=callbackFun
+2.在window上创建一个callbackfun
+3.拿到结果后将callbackfun删除掉，将script标签dom删除掉
 
-具体代码如下，最后一段是调用函数的示例，这个函数将返回一个promise对象，获取到数据时状态为resolve
+
+当请求成功后，后台返回单的内容会解析成js，于是会开始执行，后台
+服务端返回数据有特定格式要求：callback函数名+'('+JSON.stringify(返回数据)+')'
+
 */
 
 const jsonp = function (url, data) {
